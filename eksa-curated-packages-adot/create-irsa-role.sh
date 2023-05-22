f9cc901df372cb47a4ed7b940143e6a740c21549
iamprakkie <gprakk.ie@gmail.com>
2023-05-22 10:41:33 -0600

    WIP

--- eksa-curated-packages-adot/create-irsa-role.sh
+++ eksa-curated-packages-adot/create-irsa-role.sh
@@ -4,7 +4,7 @@ set -e # exit when any command fails
 
 source ./format_display.sh # format display
 source ./env-vars-check.sh # checking environment variables
-source ./ssm_send_command.sh # to send commands through ssm
+source ./ssm-send-command.sh # to send commands through ssm
 source ./deploy-pod-identity-webhook.sh # to deploy pod-identity-webhook
 
 #check for required env variables
