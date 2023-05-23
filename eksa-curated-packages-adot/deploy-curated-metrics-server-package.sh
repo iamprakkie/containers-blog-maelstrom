f795a2e7ec3dc4402a6caebea5bdb3a8417a24ea
iamprakkie <gprakk.ie@gmail.com>
2023-05-23 12:26:31 -0600

    WIP

--- eksa-curated-packages-adot/deploy-curated-metrics-server-package.sh
+++ eksa-curated-packages-adot/deploy-curated-metrics-server-package.sh
@@ -4,19 +4,11 @@ set -e # exit when any command fails
 
 source ./format-display.sh # format display
 source ./env-vars-check.sh # checking environment variables
-source ./ssm-send-command.sh # to send commands through ssm
 
 #check for required env variables
 env_vars_check
 
 NAMESPACE=${1:-observability}
-
-#configure IRSA
-sed -e "s|{{EKSA_AMP_WORKSPACE_ARN}}|${EKSA_AMP_WORKSPACE_ARN}|g" templates/amp-permission-policy-template.json > amp-permission-policy.json
-bash ./configure-irsa.sh ${NAMESPACE} "amp-permission-policy.json" ${SERVICE_ACCOUNT}
-
-#prepare curated-amp-adot-package.yaml
-
 
 #prepare curated-metrics-server-package.yaml
 sed -e "s|{{NAMESPACE}}|$NAMESPACE|g" templates/curated-metrics-server-package-template.yaml > curated-metrics-server-package.yaml
