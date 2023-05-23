0aaae47bbbb47f882bfd65b4b9c1a0aad771cc58
iamprakkie <gprakk.ie@gmail.com>
2023-05-22 20:16:57 -0600

    WIP

--- eksa-curated-packages-adot/deploy-curated-amp-adot-package.sh
+++ eksa-curated-packages-adot/deploy-curated-amp-adot-package.sh
@@ -38,53 +38,6 @@ ROLEARN=$(aws iam list-roles --query "Roles[?RoleName=='${SERVICEACCOUNT}-Role']
 sed -e "s|{{EKSA_CLUSTER_NAME}}|$EKSA_CLUSTER_NAME|g; s|{{EKSA_CLUSTER_REGION}}|$EKSA_CLUSTER_REGION|g; s|{{EKSA_AMP_REMOTEWRITE_URL}}|$EKSA_AMP_REMOTEWRITE_URL|g; s|{{NAMESPACE}}|$NAMESPACE|g; s|{{SERVICEACCOUNT}}|$SERVICE_ACCOUNT|g; s|{{ROLEARN}}|${ROLEARN}|g" templates/curated-amp-adot-package-template.yaml > curated-amp-adot-package.yaml
 
 log 'O' "Deploying curated ADOT package in namespace ${NAMESPACE}."
-bash ./deploy-manifest.sh ./curated-amp-adot-package.yaml
+bash ./deploy-manifest.sh "PACKAGE" ./curated-amp-adot-package.yaml
 
 rm -f curated-amp-adot-package.yaml
-
-#upload files to config bucket
-log 'O' "Uploading curated-amp-adot-package.yaml to ${CLUSTER_CONFIG_S3_BUCKET}."
-aws s3 cp curated-amp-adot-package.yaml s3://${CLUSTER_CONFIG_S3_BUCKET}
-
-log 'O' "Deploying curated ADOT package in NAMESPACE ${NAMESPACE}."
-MI_ADMIN_MACHINE=$(aws ssm --region ${EKSA_CLUSTER_REGION} describe-instance-information --filters Key=tag:Environment,Values=EKSA Key=tag:MachineType,Values=Admin --query InstanceInformationList[].InstanceId --output text)
-
-#create deploy-curated-amp-adot-command.json
-sed -e "s|{{CLUSTER_CONFIG_S3_BUCKET}}|${CLUSTER_CONFIG_S3_BUCKET}|g; s|{{EKSA_CLUSTER_NAME}}|${EKSA_CLUSTER_NAME}|g; s|{{EKSA_CLUSTER_REGION}}|${EKSA_CLUSTER_REGION}|g" templates/deploy-curated-amp-adot-command-template.json > deploy-curated-amp-adot-command.json
-
-ssmCommandId=$(aws ssm send-command \
-    --region ${EKSA_CLUSTER_REGION} \
-    --instance-ids ${MI_ADMIN_MACHINE} \
-    --document-name "AWS-RunShellScript" \
-    --comment "Deploying curated AMP ADOT package" \
-    --cli-input-json file://deploy-curated-amp-adot-command.json \
-    --cloud-watch-output-config "CloudWatchOutputEnabled=true,CloudWatchLogGroupName=/eksa/ssm/send-command/amp-adot" \
-    --output text --query "Command.CommandId")
-echo -e "\nSSM Command ID: ${ssmCommandId}"
-ssmCommandStatus="None"
-until [ $ssmCommandStatus == "Success" ] || [ $ssmCommandStatus == "Failed" ]; do
-    ssmCommandStatus=$(aws ssm list-command-invocations \
-        --command-id "${ssmCommandId}" \
-        --region ${EKSA_CLUSTER_REGION} \
-        --details \
-        --query "CommandInvocations[].CommandPlugins[].{Status:Status}" --output text)
-    echo $ssmCommandStatus        
-    sleep 3s # Waits 3 seconds
-done
-
-log 'O' "\nSSM Command Ouput: "
-
-aws ssm list-command-invocations \
-        --command-id "${ssmCommandId}" \
-        --region ${EKSA_CLUSTER_REGION} \
-        --details \
-        --query "CommandInvocations[].CommandPlugins[].{Output:Output}" --output text
-
-if [ $ssmCommandStatus == "Failed" ]; then
-    log 'R' "Curated ADOT deployment wth AMP FAILED. Check command output in Cloudwatch logs for more details."
-    exit 1
-else 
-    log 'G' "Curated ADOT deployment wth AMP COMPLETE!!! Check command output in Cloudwatch logs for more details."
-fi
-
-rm -f irsa-trust-policy.json irsa-amp-permission-policy.json pod-identity-webhook-auth.yaml pod-identity-webhook-deployment.yaml pod-identity-webhook-mutatingwebhook.yaml pod-identity-webhook-service.yaml deploy-pod-identity-webhook-command.json
